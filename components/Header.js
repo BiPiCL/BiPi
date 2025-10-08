f59f02a1d29b88eb4a73fa6e9be5c3ea157b3a5b
BiPiCL <bipichile2025@gmail.com>
2025-10-08 09:39:30 -0300

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,15 @@
 'use client';
 import React from 'react';
 
+const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'BiPi';
+
 export default function Header() {
   return (
     <header className="bipi-header">
       <div className="bipi-container bipi-header-inner">
         <a className="bipi-logo" href="/">
           <span className="bipi-logo-icon" aria-hidden="true">🛒</span>
-          <span className="bipi-logo-text">BiPi</span>
+          <span className="bipi-logo-text">{APP_NAME}</span>
         </a>
         <nav className="bipi-nav">
           <a className="bipi-nav-link" href="/">Inicio</a>

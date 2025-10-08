344e3f8a30a8fd46c6996f110ff55c8ac8cffe02
BiPiCL <bipichile2025@gmail.com>
2025-10-08 15:53:16 -0300

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -4,14 +4,11 @@ import React from 'react';
 export default function Header() {
   return (
     <header className="navbar">
-      <div className="nav-inner">
-        {/* Marca */}
+      <div className="container nav-inner">
         <a href="/" className="brand" aria-label="Inicio BiPi Chile">
           <span className="brand-badge" aria-hidden>🛒</span>
           <span>BiPi Chile</span>
         </a>
-
-        {/* Navegación */}
         <nav className="nav-links" aria-label="Navegación principal">
           <a href="/">Inicio</a>
           <a href="/productos">Productos</a>
